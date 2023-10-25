7d9d914bb168e9215579fa17a3d23dfa3fab077a
Malcolm <malcolm5964@gmail.com>
2023-10-25 13:50:15 +0800

    Hi

--- main.c
+++ main.c
@@ -10,82 +10,123 @@
 
 #define mbaTASK_MESSAGE_BUFFER_SIZE (60)
 
-const uint BTN_PIN_PWM_LEFT = 10;
-const uint BTN_PIN_N1 = 11;
-const uint BTN_PIN_N2 = 12;
+const uint PWM_LEFT = 10;
+const uint N1 = 11;
+const uint N2 = 12;
 
-const uint BTN_PIN_PWM_RIGHT = 15;
-const uint BTN_PIN_N3 = 14;
-const uint BTN_PIN_N4 = 13;
+const uint PWM_RIGHT = 15;
+const uint N3 = 14;
+const uint N4 = 13;
 
 const uint BTN_PIN_IR = 26;
-// const uint BTN_PIN_IR = 8;
+
+const uint WHEEL_EN_LEFT_OUT = 16;
+const uint WHEEL_EN_LEFT_VCC = 7;
+const uint WHEEL_EN_RIGHT_OUT = 24;
+const uint WHEEL_EN_RIGHT_VCC = 6;
 
 uint slice_num_right;
 uint slice_num_left;
 
 
-void leftForward()
-{
-    gpio_set_pulls(BTN_PIN_N1, true, false);
-    gpio_set_pulls(BTN_PIN_N2, true, false);
-}
-
-void leftBack()
-{
-    gpio_set_pulls(BTN_PIN_N1, false, true);
-    gpio_set_pulls(BTN_PIN_N2, false, true);
-}
-
 void Forward()
 {
-    gpio_set_pulls(BTN_PIN_N3, false, true);
-    gpio_set_pulls(BTN_PIN_N4, true, false);
-    gpio_set_pulls(BTN_PIN_N1, false, true);
-    gpio_set_pulls(BTN_PIN_N2, true, false);
+    gpio_set_pulls(N3, false, true);
+    gpio_set_pulls(N4, true, false);
+    gpio_set_pulls(N1, false, true);
+    gpio_set_pulls(N2, true, false);
 }
 
 void Back()
 {
-    gpio_set_pulls(BTN_PIN_N3, true, false);
-    gpio_set_pulls(BTN_PIN_N4, false, true);
-    gpio_set_pulls(BTN_PIN_N1, true, false);
-    gpio_set_pulls(BTN_PIN_N2, false, true);
+    gpio_set_pulls(N3, true, false);
+    gpio_set_pulls(N4, false, true);
+    gpio_set_pulls(N1, true, false);
+    gpio_set_pulls(N2, false, true);
 }
 
 void moving_task(__unused void *params)
 {
 
+    //Init Right Motor
+    gpio_set_function(PWM_RIGHT, GPIO_FUNC_PWM);
+    slice_num_right = pwm_gpio_to_slice_num(PWM_RIGHT);
+    pwm_set_clkdiv(slice_num_right, 100);
+    pwm_set_wrap(slice_num_right, 62500);
+    pwm_set_enabled(slice_num_right, true);
+    gpio_set_dir(N3, GPIO_OUT);
+    gpio_set_dir(N4, GPIO_OUT);
+
+    //Left Motor
+    gpio_set_function(PWM_LEFT, GPIO_FUNC_PWM);
+    slice_num_left = pwm_gpio_to_slice_num(PWM_LEFT);
+    pwm_set_clkdiv(slice_num_left, 100);
+    pwm_set_wrap(slice_num_left, 62500);
+    pwm_set_enabled(slice_num_left, true);  
+    gpio_set_dir(N1, GPIO_OUT);
+    gpio_set_dir(N2, GPIO_OUT);
+
     while (true)
     {
         vTaskDelay(10);
         Forward();
-        pwm_set_chan_level(slice_num_left, PWM_CHAN_A, 62500 / 2);
-        pwm_set_chan_level(slice_num_right, PWM_CHAN_B, 62500 / 2);
-        printf("ADC value: %d\n", slice_num_right);
+        pwm_set_chan_level(slice_num_left, PWM_CHAN_A, 62500/5);
+        pwm_set_chan_level(slice_num_right, PWM_CHAN_B, 62500/5);
 
-        vTaskDelay(2000);
-        pwm_set_chan_level(slice_num_left, PWM_CHAN_A, 0);
-        pwm_set_chan_level(slice_num_right, PWM_CHAN_B, 0);
-
-        vTaskDelay(2000);
-        Back();
-        pwm_set_chan_level(slice_num_left, PWM_CHAN_A, 62500 / 2);
-        pwm_set_chan_level(slice_num_right, PWM_CHAN_B, 62500 / 2);
+        //vTaskDelay(2000);
+        //pwm_set_chan_level(slice_num_left, PWM_CHAN_A, 0);
+        //pwm_set_chan_level(slice_num_right, PWM_CHAN_B, 0);
+        //
+        //vTaskDelay(2000);
+        //Back();
+        //pwm_set_chan_level(slice_num_left, PWM_CHAN_A, 62500/2);
+        //pwm_set_chan_level(slice_num_right, PWM_CHAN_B, 62500/2);
 
         vTaskDelay(2000);
     }
 }
 
-void speed_callback(uint gpio, uint32_t events)
-{
-    printf("Event: %i\n", events);
-}
 
 void measureSpeed_task(__unused void *params)
 {
 
-    gpio_set_irq_enabled_with_callback(2, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &speed_callback);
+    absolute_time_t previousTime = get_absolute_time();;
+    uint8_t edgeCounter = 0;
+    uint8_t detect_change;
+
+    //Init Left IR Sensor
+    //adc_gpio_init(WHEEL_EN_LEFT_OUT);
+    gpio_init(WHEEL_EN_LEFT_OUT);
+    gpio_set_dir(WHEEL_EN_LEFT_OUT, GPIO_IN);
+    gpio_init(WHEEL_EN_LEFT_VCC);
+    gpio_set_dir(WHEEL_EN_LEFT_VCC, GPIO_OUT);
+    gpio_put(WHEEL_EN_LEFT_VCC, 1);//Set to HIGH for VCC
+
+    //Init Right IR Sensor
+    gpio_init(WHEEL_EN_RIGHT_OUT);
+    gpio_set_dir(WHEEL_EN_RIGHT_OUT, GPIO_IN);
+    gpio_init(WHEEL_EN_RIGHT_VCC);
+    gpio_set_dir(WHEEL_EN_RIGHT_VCC, GPIO_OUT);
+    gpio_put(WHEEL_EN_RIGHT_VCC, 1);//Set to HIGH for VCC
+
+    while(true){
+
+        if(gpio_get(WHEEL_EN_LEFT_OUT) == 1) {
+            detect_change = 1;
+        }
+        if((gpio_get(WHEEL_EN_LEFT_OUT) == 0) && (detect_change == 1)) {
+            detect_change = 0;
+            edgeCounter++;
+            if(edgeCounter >= 20) {
+                edgeCounter = 0;
+                absolute_time_t current_time = get_absolute_time();
+                uint64_t revolutionTime = absolute_time_diff_us(previousTime, current_time);
+                uint64_t speed = 11000000 / revolutionTime;
+                printf("Wheel Speed: %lldcm/s\n", speed);
+                previousTime = current_time;
+            }
+        }
+    }
 }
 
 void ir_sensor()
@@ -119,11 +160,11 @@ void ir_sensor()
 
 void vLaunch(void)
 {
-    // TaskHandle_t moveCar;
-    // xTaskCreate(moving_task, "MoveCar", configMINIMAL_STACK_SIZE, NULL, 3, &moveCar);
+    TaskHandle_t moveCar;
+    xTaskCreate(moving_task, "MoveCar", configMINIMAL_STACK_SIZE, NULL, 3, &moveCar);
 
-    // TaskHandle_t measureSpeed;
-    // xTaskCreate(measureSpeed_task, "MeasureSpeed", configMINIMAL_STACK_SIZE, NULL, 3, &measureSpeed);
+    TaskHandle_t measureSpeed;
+    xTaskCreate(measureSpeed_task, "MeasureSpeed", configMINIMAL_STACK_SIZE, NULL, 3, &measureSpeed);
 
     TaskHandle_t irSensor;
     xTaskCreate(ir_sensor, "IRSensor", configMINIMAL_STACK_SIZE, NULL, 3, &irSensor);
@@ -135,25 +176,6 @@ int main(void)
 {
     stdio_init_all();
     stdio_usb_init();
-
-    // Init Right Motor
-    gpio_set_function(BTN_PIN_PWM_RIGHT, GPIO_FUNC_PWM);
-    slice_num_right = pwm_gpio_to_slice_num(BTN_PIN_PWM_RIGHT);
-    pwm_set_clkdiv(slice_num_right, 100);
-    pwm_set_wrap(slice_num_right, 62500);
-    pwm_set_enabled(slice_num_right, true);
-    gpio_set_dir(BTN_PIN_N3, GPIO_OUT);
-    gpio_set_dir(BTN_PIN_N4, GPIO_OUT);
-
-    // Left Motor
-    gpio_set_function(BTN_PIN_PWM_LEFT, GPIO_FUNC_PWM);
-    slice_num_left = pwm_gpio_to_slice_num(BTN_PIN_PWM_LEFT);
-    pwm_set_clkdiv(slice_num_left, 100);
-    pwm_set_wrap(slice_num_left, 62500);
-    pwm_set_enabled(slice_num_left, true);
-    gpio_set_dir(BTN_PIN_N1, GPIO_OUT);
-    gpio_set_dir(BTN_PIN_N2, GPIO_OUT);
-    pwm_set_chan_level(slice_num_right, PWM_CHAN_B, 62500 / 2);
 
     // Init IR Sensor
     adc_init();
